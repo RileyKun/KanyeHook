41e12e328694456d974b72cb44bc0bfc265c8fd9
dyrk <vlomov182@gmail.com>
2024-06-05 22:03:15 +0300

    remove paste

--- src/TeamFortress2/TeamFortress2/Hooks/Detours/BaseClientDLL_PostEntity.cpp
+++
@@ -1,8 +0,0 @@
-#include "../Hooks.h"
-
-MAKE_HOOK(BaseClientDLL_PostEntity, Utils::GetVFuncPtr(I::BaseClientDLL, 6), void, __fastcall,
-		  void* ecx, void* edx)
-{
-	Hook.Original<FN>()(ecx, edx);
-	
-}
--- src/TeamFortress2/TeamFortress2/Hooks/Detours/UniformRandomStream_RandInt.cpp
+++
@@ -1,7 +0,0 @@
-#include "../Hooks.h"
-
-MAKE_HOOK(UniformRandomStream_RandInt, Utils::GetVFuncPtr(I::UniformRandomStream, 2), int, __fastcall,
-		  void* ecx, void* edx, int iMinVal, int iMaxVal)
-{
-	return Hook.Original<FN>()(ecx, edx, iMinVal, iMaxVal);
-}
